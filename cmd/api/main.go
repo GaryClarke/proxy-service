63304384d16331baa1f790b6cc3d50b90e5b2b0d
Gary Clarke <garyclarke1974@gmail.com>
2025-02-16 16:10:19 +0100

    Add server, handler, logger

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,72 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"time"
+)
+
+// Later generate this automatically at build time, but for now just store the version
+// number as a hard-coded global constant.
+const version = "1.0.0"
+
+// Define a config struct to hold all the configuration settings for the application.
+type config struct {
+	port int
+	env  string
+}
+
+// Define an application struct to hold the dependencies for our HTTP handlers, helpers,
+// and middleware.
+type application struct {
+	config config
+	logger *slog.Logger
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	// Declare an instance of the config struct.
+	var cfg config
+
+	// Read the value of the port and env command-line flags into the config struct.
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.Parse()
+
+	// Initialize a new structured logger which writes log entries to the standard out
+	// stream.
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	// Declare an instance of the application struct, containing the config struct and
+	// the logger.
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	// Declare a new servemux and add a /v1/healthcheck route which dispatches requests
+	// to the healthcheckHandler method
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
+
+	// Declare a HTTP server which listens on the port provided in the config struct,
+	// uses the servemux we created above as the handler, has some sensible timeout
+	// settings and writes any log messages to the structured logger at Error level.
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      mux,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
+	}
+
+	// Start the HTTP server.
+	logger.Info("starting server", "addr", srv.Addr, "env", cfg.env)
+
+	err := srv.ListenAndServe()
+	logger.Error(err.Error())
+	os.Exit(1)
 }
