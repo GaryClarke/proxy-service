2092f395fdb727f23e9c122de2b4f17f3f896084
Gary Clarke <garyclarke1974@gmail.com>
2025-02-17 19:13:00 +0100

    Add routing

---
+++ cmd/api/routes.go
@@ -0,0 +1,13 @@
+package main
+
+import "net/http"
+
+func (app *application) routes() http.Handler {
+	// create a router (mux)
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /healthcheck", app.healthcheckHandler)
+	//mux.HandleFunc("POST /webhook", app.webhookHandler)
+
+	return mux
+}
