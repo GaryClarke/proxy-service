29ef5b23698e5ce6ff9a32d76734406016e73940
Gary Clarke <gary.clarke@immediate.co.uk>
2025-02-28 06:57:06 +0000

    Update TestHealthcheckHandler to use httptest.NewServer

--- cmd/api/handlers_test.go
+++ cmd/api/handlers_test.go
@@ -1,52 +1,44 @@
 package main
 
 import (
-	"encoding/json"
+	"bytes"
+	"github.com/garyclarke/proxy-service/internal/assert"
 	"io"
 	"log/slog"
 	"net/http"
 	"net/http/httptest"
-	"reflect"
 	"testing"
 )
 
 func TestHealthcheckHandler(t *testing.T) {
+	// Create a new instance of our application struct. For now, this just
+	// contains a structured logger (which discards anything written to it).
 	app := &application{
 		config: config{env: "testing"},
 		logger: slog.New(slog.NewTextHandler(io.Discard, nil)),
 	}
 
-	req := httptest.NewRequest("GET", "/healthcheck", nil)
-	w := httptest.NewRecorder()
+	// Use the httptest.NewServer() function to create a new test server
+	ts := httptest.NewServer(app.routes())
+	defer ts.Close()
 
-	app.healthcheckHandler(w, req)
-
-	// Check status code
-	if w.Code != http.StatusOK {
-		t.Errorf("expected status %d, got %d", http.StatusOK, w.Code)
-	}
-
-	// Check the Content-Type header.
-	if contentType := w.Header().Get("Content-Type"); contentType != "application/json" {
-		t.Errorf("expected Content-Type application/json, got %s", contentType)
-	}
-
-	// Define expected JSON as a map
-	expected := map[string]string{
-		"status":      "available",
-		"environment": "testing",
-		"version":     "1.0.0",
-	}
-
-	// Decode actual response into a map
-	var actual map[string]string
-	err := json.Unmarshal(w.Body.Bytes(), &actual)
+	// The network address that the test server is listening on is contained in
+	// the ts.URL field. We can use this along with the ts.Client().Get() method
+	// to make a GET /healthcheck request against the test server. This returns a
+	// http.Response struct containing the response.
+	rs, err := ts.Client().Get(ts.URL + "/healthcheck")
 	if err != nil {
-		t.Fatalf("failed to decode response body: %v", err)
+		t.Fatal(err)
 	}
 
-	// Compare maps instead of raw JSON strings
-	if !reflect.DeepEqual(actual, expected) {
-		t.Errorf("expected JSON %+v, got %+v", expected, actual)
+	defer rs.Body.Close()
+	body, err := io.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
 	}
+	body = bytes.TrimSpace(body)
+
+	assert.Equal(t, rs.StatusCode, http.StatusOK)
+	assert.Equal(t, rs.Header.Get("Content-Type"), "application/json")
+	assert.Equal(t, string(body), `{"environment":"testing","status":"available","version":"1.0.0"}`)
 }
