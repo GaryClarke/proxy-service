a83f87c3ed90a94b7d91ff369efa5a0c7f1bc374
Gary Clarke <gary.clarke@immediate.co.uk>
2025-03-04 09:36:59 +0000

    Create WebhookHandler inteface and delegator mechanism

---
+++ internal/webhook/handler/handler.go
@@ -0,0 +1,36 @@
+package handler
+
+import (
+	"context"
+	"github.com/garyclarke/proxy-service/internal/webhook"
+)
+
+// WebhookHandler is the interface that each handler implements.
+type WebhookHandler interface {
+	supports(wh webhook.Webhook) bool
+	handle(ctx context.Context, wh webhook.Webhook) error
+}
+
+// Delegator holds a slice of WebhookHandler.
+type Delegator struct {
+	handlers []WebhookHandler
+}
+
+// NewHandlerDelegator constructs a new HandlerDelegator.
+func NewHandlerDelegator(handlers ...WebhookHandler) *Delegator {
+	return &Delegator{
+		handlers: handlers,
+	}
+}
+
+// Delegate iterates over the handlers and calls Handle on those that support the webhook.
+func (d *Delegator) Delegate(ctx context.Context, wh webhook.Webhook) error {
+	for _, handler := range d.handlers {
+		if handler.supports(wh) {
+			if err := handler.handle(ctx, wh); err != nil {
+				return err // or log/accumulate errors as needed
+			}
+		}
+	}
+	return nil
+}
