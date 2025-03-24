572312aced37e8dd85ff4c7e212c6c4a7577af92
Gary Clarke <gary.clarke@immediate.co.uk>
2025-03-24 15:04:35 +0000

    Test additional fields and invalid payload

--- internal/webhook/handler/apple_test.go
+++ internal/webhook/handler/apple_test.go
@@ -12,9 +12,9 @@ func TestDecodeSubscriptionWebhook_ValidPayload(t *testing.T) {
 	payload := testutil.BuildSubNotesPayload(
 		"SUBSCRIBED",
 		"INITIAL_BUY",
-		"false",
 		"100000123456789",
 		"uk.co.bbc.goodfood2",
+		false,
 		1,
 	)
 
@@ -25,12 +25,31 @@ func TestDecodeSubscriptionWebhook_ValidPayload(t *testing.T) {
 	assert.NilFatalError(t, err)
 	assert.NotNil(t, sub)
 	assert.Equal(t, sub.Brand, brand.GF)
+
+	// Assert some values in nested structs.
+	assert.Equal(t, sub.Properties.MemberStatus, "active")
+	assert.Equal(t, sub.Properties.Entitlement, "APP - iOS")
+	assert.Equal(t, sub.Properties.TimePeriod, "Annual")
+
+	// Assert a value in jwsTransaction.
+	assert.NotNil(t, sub.JwsTransaction)
+	assert.Equal(t, sub.JwsTransaction.BundleID, "uk.co.bbc.goodfood2")
+	assert.Equal(t, sub.JwsTransaction.Type, "INITIAL_BUY")
+
+	// Assert a value in jwsRenewalInfo.
+	assert.NotNil(t, sub.JwsRenewalInfo)
+	assert.Equal(t, sub.JwsRenewalInfo.Environment, "Sandbox")
+
+	// Assert a value in serverData.
+	assert.NotNil(t, sub.ServerData)
+	assert.Equal(t, sub.ServerData.NotificationType, "SUBSCRIBED")
+	assert.Equal(t, *sub.ServerData.SubType, "INITIAL_BUY")
 }
 
-//func TestDecodeSubscriptionWebhook_InvalidPayload(t *testing.T) {
-//	invalidPayload := `{"invalid": "data"}`
-//	_, err := decoder.DecodeSubscriptionWebhook(invalidPayload)
-//	if err == nil {
-//		t.Fatal("expected error for invalid payload, got nil")
-//	}
-//}
+func TestDecodeSubscriptionWebhook_InvalidPayload(t *testing.T) {
+	invalidPayload := `{"invalid": "data"}`
+	_, err := DecodeSubscriptionWebhook(invalidPayload)
+	if err == nil {
+		t.Fatal("expected error for invalid payload, got nil")
+	}
+}
