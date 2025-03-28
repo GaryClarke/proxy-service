89dcdf96ae070df59b94bb1573443421ddb7a7d4
Gary Clarke <gary.clarke@immediate.co.uk>
2025-03-28 15:53:33 +0000

    Test apple and google lookups

---
+++ internal/events/events_test.go
@@ -0,0 +1,44 @@
+package events_test
+
+import (
+	"testing"
+
+	"github.com/garyclarke/proxy-service/internal/assert"
+	"github.com/garyclarke/proxy-service/internal/events"
+)
+
+func TestGetLookupData_Apple(t *testing.T) {
+	data, err := events.GetLookupData("apple")
+	assert.NilFatalError(t, err)
+	assert.NotNil(t, data)
+
+	// Check correct number of items in apple map
+	assert.Equal(t, len(data), 20)
+
+	// Verify that a specific event key exists and has expected values.
+	event, ok := data["SUBSCRIBED|INITIAL_BUY|false"]
+	if !ok {
+		t.Fatal("expected key 'SUBSCRIBED|INITIAL_BUY|false' not found")
+	}
+	assert.Equal(t, event.Name, "subscription_started")
+	assert.Equal(t, event.Category, "CATEGORY_START")
+	assert.Equal(t, *event.SubStatus, "First time subscriber")
+}
+
+func TestGetLookupData_Google(t *testing.T) {
+	data, err := events.GetLookupData("google")
+	assert.NilFatalError(t, err)
+	assert.NotNil(t, data)
+
+	// Check correct number of items in google map
+	assert.Equal(t, len(data), 14)
+
+	// Verify that a specific event key exists and has expected values.
+	event, ok := data["PURCHASED|null|false"]
+	if !ok {
+		t.Fatal("expected key 'PURCHASED|null|false' not found")
+	}
+	assert.Equal(t, event.Name, "subscription_started")
+	assert.Equal(t, event.Category, "CATEGORY_START")
+	assert.Equal(t, *event.SubStatus, "Subscriber")
+}
