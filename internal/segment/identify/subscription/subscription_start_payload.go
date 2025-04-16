20f63d917ec0504f89d501b8a04b64f6c37372c9
Gary Clarke <gary.clarke@immediate.co.uk>
2025-04-16 15:30:06 +0100

    Create SubscriptionStartPayload

--- internal/segment/identify/subscription/subscription_start_payload.go
+++ internal/segment/identify/subscription/subscription_start_payload.go
@@ -1,6 +1,7 @@
 package identify
 
 import (
+	"fmt"
 	"github.com/garyclarke/proxy-service/internal/segment"
 	"github.com/segmentio/analytics-go"
 )
@@ -20,5 +21,23 @@ type SubscriptionStartPayload struct {
 func (p *SubscriptionStartPayload) ToIdentify() analytics.Identify {
 	t := &segment.Traits{
 		Traits: make(analytics.Traits),
+	}
+	t.SetIfNotEmpty(fmt.Sprintf("acc_%s_guid", p.BrandCode), p.AccountGuid)
+	t.SetIfNotEmpty(fmt.Sprintf("app_%s_sub", p.BrandCode), p.Subscribed)
+	t.SetIfNotEmpty(fmt.Sprintf("app_%s_sub_id", p.BrandCode), p.SubscriptionID)
+	t.SetIfNotEmpty(fmt.Sprintf("app_%s_auto_renew_status", p.BrandCode), p.AutoRenewEnabled)
+	t.SetIfNotEmpty(fmt.Sprintf("%s_airship_channel_id", p.BrandCode), p.AirshipChannelID)
+	t.SetIfNotEmpty(fmt.Sprintf("acc_%s_airship_id", p.BrandCode), p.AirshipID)
+
+	c := &analytics.Context{
+		Extra: map[string]interface{}{
+			"brand_code": p.BrandCode,
+		},
+	}
+
+	return analytics.Identify{
+		UserId:  p.UserID,
+		Traits:  t.Traits,
+		Context: c,
 	}
 }
